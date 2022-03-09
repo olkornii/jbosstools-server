71fc76014c29579600fcf0ec18155e6212107a76
Oleksii Korniienko <olkornii@redhat.com>
2022-03-09 10:37:03 +0100

    update WF version for server-as-itests and server-management-itest
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- as/itests/org.jboss.tools.as.itests/src/org/jboss/tools/as/itests/EJB3SupportVerifierTest.java
+++ as/itests/org.jboss.tools.as.itests/src/org/jboss/tools/as/itests/EJB3SupportVerifierTest.java
@@ -54,6 +54,8 @@ public class EJB3SupportVerifierTest extends TestCase {
 		expected.put(IJBossToolingConstants.DEPLOY_ONLY_SERVER, false);
 		// NEW_SERVER_ADAPTER
 		// AUTOGEN_SERVER_ADAPTER_CHUNK
+		expected.put(IJBossToolingConstants.SERVER_WILDFLY_260, true);
+		expected.put(IJBossToolingConstants.SERVER_WILDFLY_250, true);
 		expected.put(IJBossToolingConstants.SERVER_WILDFLY_240, true);
 		expected.put(IJBossToolingConstants.SERVER_EAP_74, true);
 		expected.put(IJBossToolingConstants.SERVER_WILDFLY_230, true);
