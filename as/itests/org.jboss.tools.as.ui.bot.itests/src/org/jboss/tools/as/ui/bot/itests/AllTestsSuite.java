135fab1ed00d24ba700955a79832d5cb376c7a9f
Oleksii Korniienko <olkornii@redhat.com>
2022-09-23 12:50:00 +0200

    test contentShowIn
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/AllTestsSuite.java
+++ as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/AllTestsSuite.java
@@ -24,13 +24,13 @@ import org.junit.runners.Suite;
 
 @RunWith(RedDeerSuite.class)
 @Suite.SuiteClasses({ 
-	InvalidCredentialProductDownloadTest.class,
-	ServerRuntimesTest.class,
-	VariousProjectsArchiving.class,
-	DeployingArchiveTest.class,
-	RuntimeDetectionDuplicatesTest.class,
-	ServerAdaptersTest.class,
-	ServerActualJavaTest.class,
+//	InvalidCredentialProductDownloadTest.class,
+//	ServerRuntimesTest.class,
+//	VariousProjectsArchiving.class,
+//	DeployingArchiveTest.class,
+//	RuntimeDetectionDuplicatesTest.class,
+//	ServerAdaptersTest.class,
+//	ServerActualJavaTest.class,
 	ShowInContextMenuTest.class
 })
 public class AllTestsSuite {
