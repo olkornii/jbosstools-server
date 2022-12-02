5294edf88a8d2e53a8dfa9c5c7b7fedd3f0dfa09
Oleksii Korniienko <olkornii@redhat.com>
2022-12-02 19:16:28 +0100

    Revert "only runtime and rhel7"
    This reverts commit 9c2682b6e48b2156eedabc6952cbb11f76feaf5d.

--- as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/AllTestsSuite.java
+++ as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/AllTestsSuite.java
@@ -26,12 +26,12 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({ 
 //	InvalidCredentialProductDownloadTest.class,
 	ServerRuntimesTest.class,
-//	VariousProjectsArchiving.class,
-//	DeployingArchiveTest.class,
+	VariousProjectsArchiving.class,
+	DeployingArchiveTest.class,
 //	RuntimeDetectionDuplicatesTest.class,
 //	ServerAdaptersTest.class,
 //	ServerActualJavaTest.class,
-//	ShowInContextMenuTest.class
+	ShowInContextMenuTest.class
 })
 public class AllTestsSuite {
 
